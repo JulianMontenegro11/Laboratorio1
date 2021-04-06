b4e37b85de338a3561ae9c766f17d04d5630b7f0
Julian <jmontengro.pinzon111mail.com>
2021-04-05 19:51:58 -0500

    Final

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -1,72 +1,62 @@
 #include <iostream>
 #include <math.h>
 #include <cstdlib>
+#include <time.h>
 using namespace std;
 
 int main(){
 
     short a;
+    int A, B, N, C, X, resultado, fila, columna, contador1, horas, minutos, segundos, pi, res=0, res2=0;
+    int end=100, start=0;
+    double c, d, Pi=3.1416;
+    float total;
+    bool stop=false;
+    char e;
+    string f;
+
     cout << "ingresa la opcion que desea" << endl;
 
     cin >> a;
 
     switch (a) {
-        int A;
-        int B;
-        int N;
-        int C;
-        int X;
-        int resultado;
-        float perimetro;
-        float area;
-        int R;
-        int fila;
-        int columna;
-        int contador1;
-        float total;
+
         case 1:
-        int horas;
-        int minutos;
-        int segundos;
-        int pi;
+        cout << "Ejercicio 1" << endl;
 
-
-            cout << "este programa devuelve e residuo de una division";
-            cout << "ingrese A y B :";
-
-                cin >> A >> B;
-
-                cout<< "El residuo de A/B es :"<< A%B << endl;
+        cout << "Ingrese un numero a y despues un numero b:" << endl;
+        cin >> A;
+        cin >> B;
+        cout << "El el residuo de la divison " << A << "/" << B << " es: " << A%B << endl;
 
         break;
 
         case 2:
-
             cout << " Este orograma nos dice si un numero es par o impar"<<endl;
             cout << "Ingrese un numero:"<<endl;
-                cin >> N;
+            cin >> N;
 
-                if (N%2 == 0){
-                    cout << "El numero es par";
-                    cout << endl;
-                }
+            if (N%2 == 0){
+                cout << "El numero es par";
+                cout << endl;
+            }
         break;
 
         case 3:
+        cout << "Ejercicio 3" << endl;
 
-            cout << "Este programa nos dice cuel es e numero mas grande entre dos ingresados"<<endl;
-            cout << "Ingrese os numeros A y B:"<<endl;
-            cin >> A;
-            cin >> B;
-
-           if (A < B){
-               cout << B << " es mayor" << endl;
-           }
-
-           else{
-               cout << A <<" es mayor" << endl;
-              }
-
+        cout << "Ingrese un numero a y despues un numero b:" << endl;
+        cin >> A;
+        cin >> B;
+        if (A>B) {
+            cout << "El mayor es: " << A << endl;
+        }
+        else if (A<B) {
+            cout << "El mayor es: " << B << endl;
+        }
+        else {
+            cout << "Los numeros son iguales '" << A << " = " << B << "'." << endl;
+        }
         break;
 
         case 4:
@@ -79,58 +69,59 @@ int main(){
             else{
                 cout<<"El numero menor es "<<B<<endl;
             }
-
         break;
 
         case 5:
-            cout<<"Este programa entrega la parte entera de una division"<<endl;
-            cout<<"Ingrese los numeros A y B"<<endl;
-            cin>>A>>B;
+        cout << "Ejercicio 5" << endl;
 
-            C=A/B;
+        cout << "Ingrese un numero a y despues un numero b:" << endl;
+        cin >> c;
+        cin >> d;
 
-            cout<<C<<endl;
+        if ((c/d - int(c/d)) >= double (0.5)){
+            cout << c << "/" << d << "=" << int (c/d) + 1 << endl;
+        }
+        else {
+            cout << c << "/" << d << "=" << int (c/d) << endl;
+        }
+        break;
 
+        case 6:
+        resultado=0;
+        cout<<"Este programa entrega la potencia de A elevado a la B"<<endl;
+        cout<<"Ingrese la base"<<endl;
+        cin>>A;
 
-         break;
+        cout<<"Ingrese el exponente"<<endl;
+        cin>>B;
 
-         case 6:
+        X=1;
 
-            resultado=0;
-            cout<<"Este programa entrega la potencia de A elevado a la B"<<endl;
-            cout<<"Ingrese la base"<<endl;
-            cin>>A;
-
-            cout<<"Ingrese el exponente"<<endl;
-            cin>>B;
-
-           X=1;
-
-            while (X<B){
-                if(resultado<A){
-                    resultado=A*A;
-                }
-                else{
-                    resultado=resultado*A;
-                }
-                X++;
+        while (X<B){
+            if(resultado<A){
+                resultado=A*A;
             }
-            cout<< resultado<<endl;
+            else{
+                resultado=resultado*A;
+            }
+            X++;
+        }
+        cout<< resultado<<endl;
+        break;
 
-            break;
         case 7:
-            cout<<"Este programa permite realizar la sumatoria desde 0 hasta el numero 'N' ingresado"<<endl;
-            resultado=0;
-            X=1;
-            cout <<"Ingrese el numero N"<<endl;
-            cin>>N;
-            while(X<=N){
-                resultado=resultado+X;
-                X++;
-            }
-            cout<<resultado<<endl;
-            break;
-         case 8:
+        cout << "Ejercicio 7" << endl;
+
+        cout << "Ingrese un numero N: ";
+        cin >> A;
+
+        for (int i=0; i<=A; i++){
+            res += i;
+        }
+        cout << "La sumatoria desde 0 hasta " << A << " es: " << res << endl;
+        break;
+
+        case 8:
             cout<<"Este programa muestra el factorial de un numero N ingresado"<<endl;
             resultado=1;
             X=1;
@@ -143,18 +134,17 @@ int main(){
             }
             cout<<resultado<<endl;
             break;
-          case 9:
-            cout<<"Este programa muestra el perimetro y el area de un circulo al ingresar el valor del radio"<<endl;
-            cout<<"Ingrese el readio N"<<endl;
-            cin>>N;
-            perimetro= 2*3.1416*N;
-            R=N*N;
-            area=3.1416*R;
 
-            cout<<"El perimetro es: "<<perimetro<<endl;
-            cout<<"El area es: "<<area<<endl;
+        case 9:
+        cout << "Ejercicio 9" << endl;
 
-            break;
+        cout << "Ingrese un numero N correspondiente al radio de un circulo: " << endl;
+        cin >> c;
+
+        cout << "Perimetro: " << 2.0*Pi*c << endl;
+        cout << "Area: " << Pi*c*c << endl;
+        break;
+
           case 10:
             cout<<"Este programa muestra todos los multiplos menores de 100 del numero N ingresado"<<endl;
             cout<<"Ingrese el numero N"<<endl;
@@ -169,16 +159,18 @@ int main(){
             }
 
             break;
+
         case 11:
-            cout<<"Este programa entrega las tablas de multiplicar del numero ingresado hasta el 10"<<endl;
-            cout<<"Ingresa el numero"<<endl;
-            cin>>N;
-            X=1;
-            while(X<=10){
-                cout<<N<<"X"<<X<<"="<<N*X<<endl;
-                X++;
-            }
-            break;
+        cout << "Ejercicio 11" << endl;
+
+        cout << "Ingrese un numero N: " << endl;
+        cin >> A;
+
+        for (int i=1; i<=10; i++){
+            cout << i << "x" << A << "=" << i*A << endl;
+        }
+        break;
+
          case 12:
             cout<<"Este programa entrega la potencia de un numero ingresado de grado 1 hasta el grado 5"<<endl;
             cout<<"Ingrese el numero N"<<endl;
@@ -191,19 +183,21 @@ int main(){
             }
 
             break;
+
         case 13:
-            cout<<"Este programa nos muestra cuales son los divisores de un numero N ingresado"<<endl;
-            cout<<"Ingrese el numero N"<<endl;
-            cin>>N;
-            X=1;
-            cout<<"los Divisores son:"<<endl;
-            while(X<=N){
-                if(N%X==0){
-                    cout<<X<<endl;
-                }
-                X++;
+        cout << "Ejercicio 13" << endl;
+
+        cout << "Ingrese un numero N: " << endl;
+        cin >> A;
+
+        cout << "Los divisores de " << A << " son:" << endl;
+        for (int i=1; i<=A; i++){
+            if (A%i==0) {
+                cout << i << endl;
             }
-            break;
+        }
+        break;
+
         case 14:
             fila=1;
             while (fila<=50) {
@@ -211,7 +205,6 @@ int main(){
                 while (columna<=3){
                     if(columna==1){
                         cout<<fila;
-
                     }
                     else {
                         if(columna==2){
@@ -221,17 +214,27 @@ int main(){
                             cout<<51-fila;
                             cout<<endl;
                         }
-
                     }
                   columna++;
-
-
-
                 }
                 fila++;
               }
             break;
 
+        case 15:
+        cout << "Ejercicio 15" << endl;
+
+        do {
+            cout << "Ingrese un numero: ";
+            cin >> A;
+            res += A;
+            if (A==0) {
+                stop = true;
+            }
+        }
+        while (stop == false);
+        cout << "El resultado de la sumatoria es: " << res << endl;
+        break;
 
         case 16:
             cout<<"Este programa solicita constente numeros al usuario, cualdo el usuario ingresa el numero 0 el programa nos entrega el promedio de todos los numeros ingresados"<<endl;
@@ -256,8 +259,22 @@ int main(){
 
             break;
 
+        case 17:
+        cout << "Ejercicio 17" << endl;
 
-
+        do {
+            cout << "Ingrese un numero: ";
+            cin >> A;
+            if (A > res) {
+                res = A;
+            }
+            if (A==0) {
+                stop = true;
+            }
+        }
+        while (stop == false);
+        cout << "El numero mayor fue: " << res << endl;
+        break;
 
         case 18:
             cout<<"Este programa solicita un numero al usuario y nos indica si es el numero es un cuadrado perfecto"<<endl;
@@ -282,15 +299,25 @@ int main(){
                 contador1++;
 
             }
-
-
-
             break;
 
+        case 19:
+        cout << "Ejercicio 9" << endl;
 
+        cout << "Ingrese un numero N: ";
+        cin >> A;
 
-
-
+        for (int i=2; i<=A; i++) {
+            res = A%i;
+            if (res==0 && i!=A) {
+                cout << A << " NO es un numero primo." << endl;
+                break;
+            }
+            if (i==A) {
+                cout << A << " es un numero primo." << endl;
+            }
+        }
+        break;
 
         case 20:
             cout<<"Este programa solicita un numero N al usuario y le indica si es un numero polindromo"<<endl;
@@ -313,7 +340,19 @@ int main(){
 
             break;
 
+        case 21:
+        cout << "Ejercicio 21" << endl;
 
+        cout << "Ingrese una letra:" << endl;
+        cin >> e;
+
+        if (islower(e)) {
+            cout << "Letra convertida: " << char (toupper(e)) << endl;
+        }
+        else {
+            cout << "Letra convertida: " << char (tolower(e)) << endl;
+        }
+        break;
 
         case 22:
 
@@ -337,10 +376,26 @@ int main(){
             }
 
             cout<<"El tiempo es:  "<<horas<<":"<<minutos<<":"<<segundos<<endl;
-
-
-
             break;
+
+        case 23:
+        cout << "Ejercicio 23" << endl;
+
+        cout << "Ingrese un numero A y luego un numero B: " << endl;
+        cin >> A;
+        cin >> B;
+
+        for (int i=1; stop==false; i++) {
+            res = A*i;
+            for (int j=1; res2<=res; j++) {
+                res2 = B*j;
+                if (res == res2) {
+                    stop = true;
+                }
+            }
+        }
+        cout << "El MCM de " << A << " y " << B << " es: " << res << endl;
+        break;
 
 
 
@@ -370,8 +425,21 @@ int main(){
             }
             break;
 
+        case 25:
+        cout << "Ejercicio 25" << endl;
 
+        cout << "Ingrese un numero: " << endl;
+        cin >> B;
+        A = B;
 
+        while (A>0) {
+            A /= 10;
+            res +=1;
+        }
+
+        if (B == 0) cout << B << " tiene 1 digito." << endl;
+        else cout << B << " tiene " << res << " digitos." << endl;
+        break;
 
         case 26:
             cout<<"Este programa indica que tipo de triangulo forman tres longitudes que ingresa el usuario"<<endl;
@@ -399,10 +467,31 @@ int main(){
                     }
                 }
             }
-
-
             break;
 
+        case 27:
+        cout << "Ejercicio 27" << endl;
+
+        cout << "Ingrese un numero: ";
+        cin >> A;
+        cout << "Ingrese '+' para sumar, '-' para restar, '*' para multiplicar o '/' para dividir: ";
+        cin >> f;
+        cout << "Ingrese otro numero: ";
+        cin >> B;
+
+        if (f=="+") {
+            cout << A << "+" << B << "=" << A+B << endl;
+        }
+        else if (f=="-") {
+            cout << A << "-" << B << "=" << A-B << endl;
+        }
+        else if (f=="*") {
+            cout << A << "x" << B << "=" << A*B << endl;
+        }
+        else if (f=="/") {
+            cout << A << "/" << B << "=" << A/B << endl;
+        }
+        break;
 
         case 28:
 
@@ -412,12 +501,40 @@ int main(){
             cout << "La aproximacion de pi con n=" << N << " es " << 4*pi << endl;
             break;
 
+    case 29:
+    cout << "Ejercicio 29" << endl;
+
+    do {
+        srand(time(NULL));
+        A = rand() % (end-start+1)+start;
+
+        cout << "ingrese '<' si el numero pensado es menor." << endl;
+        cout << "ingrese '>' si el numero pensado es mayor." << endl;
+        cout << "ingrese '=' si el numero pensado es el que esta en pantalla." << endl << endl;
+
+        cout << A << endl;
+        cin >> f;
+
+        if (f=="<") {
+            end = A;
+        }
+        else if (f==">") {
+            start = A;
+        }
+        else {
+            stop = true;
+        }
+    }
+    while (stop==false);
+    cout << "Tu numero pensado es: " << A << endl;
+    break;
 
        case 30:
-            srand((NULL));
+        contador1 = 0;
+            srand(time(NULL));
                         A = rand() % 100 + 0;
                         do{
-                            contador1= 1;
+                            contador1++;
                             cout <<"Ingrese un numero"<< endl;
                             cin >> B;
                             if(A > B){cout << "A es mayor!\n";}
@@ -426,9 +543,8 @@ int main(){
                         }
                         while(A != B);
             break;
-
-
-
-
     }
+    cout << "\n\n\nTrabajo hecho por:" << endl;
+    cout << char(26) << "Julian Montenegro." << endl;
+    cout << char(26) << "Juan Diego Carrera.\n\n" << endl;
 }
